8aa7fe8eba7d548800119664ba27c67afda63ec8
Julien Berard <jujugrrr@gmail.com>
2015-05-22 09:48:40 +0100

    Added more details to metadata

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,8 @@ description 'Installs/Configures rackspace_monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
+issues_url 'https://github.com/rackspace-cookbooks/rackspace_monitoring/issues'
+source_url 'https://github.com/rackspace-cookbooks/rackspace_monitoring'
+
 depends 'apt'
 depends 'yum'
